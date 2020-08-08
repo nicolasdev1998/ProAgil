cfb27d2baf883c304ac124d67571004b17a32af4
Nicolas Soares Note <nicolasoliveirasoares@gmail.com>
2020-08-08 19:25:42 -0300

    Inserção projeto Angular SPA

--- ProAgil.WebApi/Startup.cs
+++ ProAgil.WebApi/Startup.cs
@@ -22,6 +22,7 @@ namespace ProAgil.WebApi
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +39,7 @@ namespace ProAgil.WebApi
             }
 
             app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             app.UseMvc();        
         }
     }
